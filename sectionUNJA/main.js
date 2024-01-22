541a520f974226bc43b7aed87ca9a025c62e9eb9
Momo <mohandguellal.dev@gmail.com>
2024-01-22 15:53:33 +0100

    1er

---
+++ sectionUNJA/main.js
@@ -0,0 +1,8 @@
+let docsent = document.querySelector("#sentinel");
+let cotricolent = document.querySelector("#ecotricol");
+docsent.addEventListener('click', () =>{
+    window.open("images/sent.pdf");
+});
+cotricolent.addEventListener('click', () =>{
+    window.open("images/sent.pdf");
+})
