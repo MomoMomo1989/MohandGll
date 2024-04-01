b1cf58596a0f21261450811d1133266f76b0f16c
Momo <mohandguellal.dev@gmail.com>
2024-04-01 15:18:19 +0100

    354

---
+++ Quiz js/index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Sources/styleIndex.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <title>Connectez Vous</title>
+</head>
+<body>
+   <div class="conteneur">
+    <div class="contenu">
+    <form>
+        <p>
+          <label class="textinput" for="Pseudo"> User</label>
+          <input type="text" class="champs1">  
+           </br>
+           </br>
+          <label class="textinput2" for="MDP"> Mot de passe</label>
+          <input type="text" class="champs2">
+        </p>
+        <input class="Boutton" type="button" name="Connexion" value="Se connecter">
+    </form>
+    </div>
+   </div>
+</body>
+</html>
